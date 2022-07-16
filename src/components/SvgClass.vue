81a60b96e050ad205eda9dca1efa17601ec20e65
Rob van der Capellen <rob.vandercapellen@innofactor.com>
2022-07-16 12:26:23 +0300

    Add Path to line

--- src/components/SvgClass.vue
+++ src/components/SvgClass.vue
@@ -11,38 +11,53 @@
     >
       <line x1="0" y1="600" x2="600" y2="600" stroke="black" stroke-width="3" />
       <line x1="0" y1="0" x2="0" y2="600" stroke="black" stroke-width="3" />
+      <path :d="path" stroke="red" stroke-width="3" fill="red" />
       <circle
         v-for="p in lo"
         :cx="p.x"
         :cy="p.y"
-        r="1"
-        stroke="red"
+        :r="circleSize"
+        stroke="black"
         stroke-width="3"
-        fill="red"
+        fill="black"
       />
     </svg>
   </div>
 </template>
 
 <script lang="ts">
-import { findDir } from "@vue/compiler-core";
 import { defineComponent } from "vue";
-import { check_a_point } from "../utils/graphical.ts";
+import { point_overlap } from "../utils/graphical";
 
-declare interface Point {
+export declare interface Point {
   x: string;
   y: string;
 }
+
 export default defineComponent({
   data: () => {
     return {
       lo: [{ x: "50", y: "50" }] as Point[],
       mouseDown: false,
       selectedPoint: undefined as Point | undefined,
+      circleSize: 5,
     };
   },
-
+  computed: {
+    path() {
+      return this.buildPath();
+    },
+  },
   methods: {
+    buildPath() {
+      const path = this.lo.reduce((previous, current) => {
+        if (previous === "") return `M${current.x} ${current.y}`; //`M{current.x} {current.y}`
+        return `${previous} L${current.x} ${current.y}`;
+      }, ``);
+      console.log("Path: ", path);
+
+      return path;
+    },
     handleMouseOut(e: MouseEvent) {
       console.log("Mouse out");
       //this.mouseDown = false;
@@ -53,12 +68,12 @@ export default defineComponent({
 
       if (this.selectedPoint) {
         if (
-          check_a_point(
+          point_overlap(
             currentPoint.x,
             currentPoint.y,
-            this.selectedPoint.x,
-            this.selectedPoint.y,
-            3
+            +this.selectedPoint.x,
+            +this.selectedPoint.y,
+            this.circleSize
           )
         ) {
           const i = this.lo.indexOf(this.selectedPoint);
@@ -69,7 +84,13 @@ export default defineComponent({
         console.log("Index:", i);
         this.lo.splice(i, 1);
         const found = this.lo.find((l) => {
-          return check_a_point(l.x, l.y, currentPoint.x, currentPoint.y, 3);
+          return point_overlap(
+            +l.x,
+            +l.y,
+            currentPoint.x,
+            currentPoint.y,
+            this.circleSize
+          );
         });
         if (!found) this.lo.splice(i, 0, currentPoint);
       } else this.lo.push(currentPoint);
@@ -84,7 +105,7 @@ export default defineComponent({
       this.mouseDown = true;
       console.log("testing: ", e.layerX, e.layerY);
       const found = this.lo.find((l) => {
-        return check_a_point(l.x, l.y, e.layerX, e.layerY, 3);
+        return point_overlap(+l.x, +l.y, e.layerX, e.layerY, this.circleSize);
       });
       if (found) this.selectedPoint = found;
       console.log("testing: ", this.lo);
--- src/utils/graphical.ts
+++ src/utils/graphical.ts
@@ -1,14 +1,23 @@
-export function check_a_point(
+export function point_overlap(
   a: number,
   b: number,
   x: number,
   y: number,
   r: number
 ) {
-  const dist_points = (a - x) * (a - x) + (b - y) * (b - y);
+  const dist_points = distanceBetweenPoints(a, b, x, y);
   r *= r;
   if (dist_points < r) {
     return true;
   }
   return false;
+}
+
+export function distanceBetweenPoints(
+  a: number,
+  b: number,
+  x: number,
+  y: number
+) {
+  return (a - x) * (a - x) + (b - y) * (b - y);
 }
